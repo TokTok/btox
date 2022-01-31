e5460c6fda08361805ef346960c7f6990f64e25c
Robin Linden <dev@robinlinden.eu>
2022-01-31 22:50:14 +0100

    Create a wasm Tox and try to bootstrap it

--- lib/ffi/init_web.dart
+++ lib/ffi/init_web.dart
@@ -4,11 +4,15 @@ import 'package:inject_js/inject_js.dart' as js;
 import 'package:web_ffi/web_ffi.dart';
 import 'package:web_ffi/web_ffi_modules.dart';
 
+import 'toxcore_generated_bindings.dart';
+
 Module? _module;
 
 Future<void> initFfi() async {
   assert(_module == null);
   Memory.init();
+  registerOpaqueType<Tox>();
+  registerOpaqueType<Tox_Options>();
   await js.importLibrary('asset/libtoxcore.js');
   String path = 'asset/libtoxcore.wasm';
   Uint8List wasmBinaries = (await rootBundle.load(path)).buffer.asUint8List();

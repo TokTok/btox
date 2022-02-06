c96f8ca41c119af49c799f08c15b429068415b6b
jfreegman <jfreegman@gmail.com>
2022-02-06 16:41:58 -0500

    Add settings and profile skeleton pages
    The quit button should now work as well

---
+++ lib/settings.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  final String title = 'Settings';
+
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+    );
+  }
+}
